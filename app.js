668eac9c77dc9bcf947cb21faaed5e00cf34f324
Cedrenou <ced2411@gmail.com>
2019-11-29 13:24:36 +0100

    disable bufferCommands

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use((req, res) => {
 const url = 'mongodb+srv://Cedric:DartDB@dart-cluster-ckd4d.gcp.mongodb.net/test?retryWrites=true&w=majority'
 const devUri = 'mongodb://localhost:27017/dart-tournament'
 
+mongoose.set('bufferCommands', false)
+
 mongoose.connect(url, {useNewUrlParser: true})
 connection.on('error', (err) => {
 	console.error(`Connection to mongoDB error : ${err.message}`)
