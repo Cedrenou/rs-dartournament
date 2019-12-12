abd77112863f5548f1a7601409c13f7ee5567ecf
Cedrenou <ced2411@gmail.com>
2019-12-12 13:21:20 +0100

    add requireHTTPS middleware

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ const playersRouter = require('./api/v1/players')
 
 app.set('port', (process.env.MONGODB_URI || 5000))
 
+function requireHTTPS(req, res, next) {
+	// The 'x-forwarded-proto' check is for Heroku
+	if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+		return res.redirect('https://' + req.get('host') + req.url);
+	}
+	next();
+}
+
+
+app.use(requireHTTPS())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
